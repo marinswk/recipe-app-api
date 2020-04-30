1c06f445c8e353dfb9cf5f5af1c75c1a01049f75
marinswk <marins.wk@gmail.com>
2020-04-30 12:19:21 +0200

    added upload image endpoint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -35,7 +35,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = (
             'id', 'title', 'ingredients', 'tags', 'time_minutes',
-            'price', 'link'
+            'price', 'link', 'image'
             )
         read_only_fields = ('id',)
 
@@ -44,3 +44,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """serializer for recipe detail object"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """serializer to upload images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id', )
